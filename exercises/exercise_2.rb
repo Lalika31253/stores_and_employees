2e1e39a4abfbaffce22313f7fa5a061c121ab433
Lalika31253 <Lalika31253@gmail.com>
2024-04-08 19:51:33 -0600

    Update the first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,15 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1) # Retrieve store with ID 1 from the database
+@store2 = Store.find_by(id: 2)
+
+puts @store1.inspect #print all object
+
+@store1.update(name: "NewName") #update name
+
+puts @store1.inspect
+
+
+
+
